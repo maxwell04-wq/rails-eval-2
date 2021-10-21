57cc5c77079b44934c6a2cf76485bba199de7fe0
maxwell04-wq <59219220+maxwell04-wq@users.noreply.github.com>
2021-10-21 12:11:23 +0500

    Update add_project_spec.rb

--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe "adding a project", type: :system do
   it "allows a user to create a project with tasks" do
     visit new_project_path
     fill_in "Name", with: "Project Runway"
-    fill_in "Tasks", with: "Choose Fabric:3\nMake it Work:5" click_on("Create Project")
+    fill_in "Tasks", with: "Choose Fabric:3\nMake it Work:5" 
+    click_on("Create Project")
     visit projects_path
-    expect(page).to have_content("Project Runway") expect(page).to have_content(8)
+    expect(page).to have_content("Project Runway") 
+    expect(page).to have_content(8)
   end 
 end
