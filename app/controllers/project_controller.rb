7eb01e6d73eac67804ec1d8e46c390a150b1e5dd
maxwell04-wq <59219220+maxwell04-wq@users.noreply.github.com>
2021-10-21 15:49:31 +0500

    Update project_controller.rb

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -2,4 +2,11 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end 
+  def create
+    @workflow = CreatesProject.new(
+      name: params[:project][:name],
+      task_string: params[:project][:tasks]) 
+    @workflow.create
+    redirect_to projects_path
+  end
 end
